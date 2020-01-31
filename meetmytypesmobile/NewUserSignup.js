6f71945b0b61126a5c441906ab2eb18a61df3f86
Jason Brunelle <jason@jasonbrunelle.com>
2020-01-31 14:43:31 -0600

    Add current city and current state Hook up full name, nick name, DOB & TOB to state Correct DateTimePicker code so it works properly

--- meetmytypesmobile/NewUserSignup.js
+++ meetmytypesmobile/NewUserSignup.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react'
-import {View, Text, TextInput, StyleSheet, Button, Picker} from 'react-native';
+import {View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class NewUserSignup extends Component {
     state = {
-        date: new Date('2020-06-12T14:42:42'),
+        fullName: '',
+        nickName: '',
+        dateOfBirth: '',
+        timeOfBirth: '',
+        gender: '',
+        cityOfBirth: '',
+        stateOfBirth: '',
+        currentCity: '',
+        currentState: '',
+        occupation: '',
+        specialGift: '',
+        email: '',
+        password: '',
+        date_time: new Date(),
         mode: 'date',
         show: false,
     }
-    setDate = (event, date) => {
-        date = date || this.state.date;
 
-        this.setState({
-            show: Platform.OS === 'ios' ? true : false,
-            date
-        });
+    formatTime = date_time => {
+        let hours = date_time.getHours();
+        const minutes = date_time.getMinutes();
+        let am_pm = "AM";
+        if (hours >= 12) {
+            am_pm = "PM"
+        }
+        if (hours >= 13) {
+            hours -= 12;
+        }
+        if (hours === 0) {
+            hours = 12;
+        }
+        return `${hours.toString().padStart(2)}:${minutes.toString().padStart(2)} ${am_pm}`;
+    }
+
+    setDate = (event, date) => {
+        if (this.state.mode === 'date') {
+            this.setState({
+                show: Platform.OS === 'ios' ? true : false,
+                dateOfBirth: (new Date(date)).toLocaleDateString()
+            });    
+        } else {
+            this.setState({
+                show: Platform.OS === 'ios' ? true : false,
+                timeOfBirth: this.formatTime(new Date(date))
+            });    
+        }
     }
 
     show = mode => {
@@ -24,25 +59,24 @@ export default class NewUserSignup extends Component {
         });
     }
 
-    datepicker = () => {
+    datePicker = () => {
         this.show('date');
     }
     
-    timepicker = () => {
+    timePicker = () => {
         this.show('time');
     }
 
     render() {
-        const { show, date, mode } = this.state;
         return (
-            <View style={styles.mainCol}>
+            <ScrollView contentContainerStyle={styles.mainCol}>
                 <View style={styles.row1Label}>
                     <Text style={styles.nameLabel}>Full Name</Text>
                     <Text style={styles.nameLabel}>Nickname (Shown to others)</Text>
                 </View>             
                 <View style={styles.row1Input}>
-                    <TextInput style={styles.nameInput} />
-                    <TextInput style={styles.nameInput} />
+                    <TextInput style={styles.nameInput} value={this.state.fullName} onChange={(e) => this.setState({fullName: e.text})} />
+                    <TextInput style={styles.nameInput} value={this.state.nickName} onChange={(e) => this.setState({nickName: e.text})} />
                 </View>             
                 <View style={styles.row2Label}>
                     <Text style={styles.dobLabel}>Date of Birth</Text>
@@ -50,11 +84,12 @@ export default class NewUserSignup extends Component {
                     <Text style={styles.genderLabel}>Gender</Text>
                 </View>             
                 <View style={styles.row2Input}>
-                    <Text style={styles.dobInput} onPress={this.datepicker} title="Show date picker!"></Text>
-                    <Text style={styles.timeInput} onPress={this.timepicker} title="Show date picker!"></Text>
-                    { show && <DateTimePicker value={date}
-                        mode={mode}
-                        is24Hour={true}
+                    <Text style={styles.dobInput} value={this.state.dateOfBirth} onPress={this.datePicker}>{this.state.dateOfBirth}</Text>
+                    <Text style={styles.timeInput} value={this.state.timeOfBirth} onPress={this.timePicker}>{this.state.timeOfBirth}</Text>
+                    { this.state.show && <DateTimePicker
+                        value={this.state.date_time}
+                        mode={this.state.mode}
+                        is24Hour={false}
                         display="default"
                         onChange={this.setDate} />
                     }
@@ -69,29 +104,39 @@ export default class NewUserSignup extends Component {
                     <TextInput style={styles.stateOfBirthInput} />
                 </View>             
                 <View style={styles.row4Label}>
+                    <Text style={styles.currentCityLabel}>Current City</Text>
+                    <Text style={styles.currentStateLabel}>Current State</Text>
+                </View>             
+                <View style={styles.row4Input}>
+                    <TextInput style={styles.currentCityInput} />
+                    <TextInput style={styles.currentStateInput} />
+                </View>             
+                <View style={styles.row5Label}>
                     <Text style={styles.occupationLabel}>Occupation</Text>
                     <Text style={styles.specialGiftLabel}>Special Gift</Text>
                 </View>             
-                <View style={styles.row4Input}>
+                <View style={styles.row5Input}>
                     <TextInput style={styles.occupationInput} />
                     <TextInput style={styles.specialGiftInput} />
                 </View>             
-                <View style={styles.row5Label}>
+                <View style={styles.row6Label}>
                     <Text style={styles.emailLabel}>Email Address (username)</Text>
                 </View>             
-                <View style={styles.row5Input}>
+                <View style={styles.row6Input}>
                     <TextInput style={styles.emailInput} />
                 </View>             
-                <View style={styles.row6Label}>
+                <View style={styles.row7Label}>
                     <Text style={styles.passwordLabel}>Password</Text>
                 </View>             
-                <View style={styles.row6Input}>
+                <View style={styles.row7Input}>
                     <TextInput style={styles.passwordInput} />
                 </View>             
-                <View style={styles.row7}>
-                    <View style={styles.registerInput}><Text style={styles.registerText}>Register</Text></View>
+                <View style={styles.row8}>
+                    <TouchableOpacity style={styles.registerInput}>
+                        <Text style={styles.registerText}>Register</Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
+            </ScrollView>
         )
     }
 }
@@ -197,6 +242,13 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
+    row4Label: {
+        flexDirection: 'row',
+        marginTop: 20
+    },
+    row4Input: {
+        flexDirection: 'row'
+    },
     stateOfBirthLabel: {
         width: '45%',
         alignItems: 'center',
@@ -212,11 +264,41 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
-    row4Label: {
+    currentCityLabel: {
+        width: '45%',
+        alignItems: 'center',
+        color: '#2699FB',
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    currentCityInput: {
+        width: '45%',
+        alignItems: 'center',
+        borderColor: '#BCE0FD',
+        borderWidth: 1,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    currentStateLabel: {
+        width: '45%',
+        alignItems: 'center',
+        color: '#2699FB',
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    currentStateInput: {
+        width: '45%',
+        alignItems: 'center',
+        borderColor: '#BCE0FD',
+        borderWidth: 1,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    row5Label: {
         flexDirection: 'row',
         marginTop: 20
     },
-    row4Input: {
+    row5Input: {
         flexDirection: 'row'
     },
     occupationLabel: {
@@ -249,11 +331,11 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
-    row5Label: {
+    row6Label: {
         flexDirection: 'row',
         marginTop: 20
     },
-    row5Input: {
+    row6Input: {
         flexDirection: 'row'
     },
     emailLabel: {
@@ -271,11 +353,11 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
-    row6Label: {
+    row7Label: {
         flexDirection: 'row',
         marginTop: 20
     },
-    row6Input: {
+    row7Input: {
         flexDirection: 'row'
     },
     passwordLabel: {
@@ -293,7 +375,7 @@ const styles = StyleSheet.create({
         marginLeft: 15,
         marginRight: 15
     },
-    row7: {
+    row8: {
         flexDirection: 'row',
         marginTop: 20
     },
