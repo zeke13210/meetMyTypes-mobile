4de8b509e05b2a1733eb7dc63c8b72710f4310d5
Jason Brunelle <jason@jasonbrunelle.com>
2020-01-31 15:02:42 -0600

    Add KeyboardAvoidingView Finish hooking up all components to state Populate all pickers

--- meetmytypesmobile/NewUserSignup.js
+++ meetmytypesmobile/NewUserSignup.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react'
-import {View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity, Picker, KeyboardAvoidingView } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class NewUserSignup extends Component {
     state = {
+        genders: [
+            {label: 'Male', value: 'male'},
+            {label: 'Female', value: 'female'},
+            {label: 'Non-binary', value: 'non-binary'},
+        ],
+        states: [
+            {label: 'Alabama', value: 'Alabama'},
+            {label: 'Alaska', value: 'Alaska'},
+            {label: 'Arizona', value: 'Arizona'},
+            {label: 'Arkansas', value: 'Arkansas'},
+            {label: 'California', value: 'California'},
+            {label: 'Colorado', value: 'Colorado'},
+            {label: 'Connecticut', value: 'Connecticut'},
+            {label: 'Delaware', value: 'Delaware'},
+            {label: 'Florida', value: 'Florida'},
+            {label: 'Georgia', value: 'Georgia'},
+            {label: 'Hawaii', value: 'Hawaii'},
+            {label: 'Idaho', value: 'Idaho'},
+            {label: 'Illinois', value: 'Illinois'},
+            {label: 'Indiana', value: 'Indiana'},
+            {label: 'Iowa', value: 'Iowa'},
+            {label: 'Kansas', value: 'Kansas'},
+            {label: 'Kentucky', value: 'Kentucky'},
+            {label: 'Louisiana', value: 'Louisiana'},
+            {label: 'Maine', value: 'Maine'},
+            {label: 'Maryland', value: 'Maryland'},
+            {label: 'Massachusetts', value: 'Massachusetts'},
+            {label: 'Michigan', value: 'Michigan'},
+            {label: 'Minnesota', value: 'Minnesota'},
+            {label: 'Mississippi', value: 'Mississippi'},
+            {label: 'Missouri', value: 'Missouri'},
+            {label: 'Montana', value: 'Montana'},
+            {label: 'Nebraska', value: 'Nebraska'},
+            {label: 'Nevada', value: 'Nevada'},
+            {label: 'New Hampshire', value: 'New Hampshire'},
+            {label: 'New Jersey', value: 'New Jersey'},
+            {label: 'New Mexico', value: 'New Mexico'},
+            {label: 'New York', value: 'New York'},
+            {label: 'North Carolina', value: 'North Carolina'},
+            {label: 'North Dakota', value: 'North Dakota'},
+            {label: 'Ohio', value: 'Ohio'},
+            {label: 'Oklahoma', value: 'Oklahoma'},
+            {label: 'Oregon', value: 'Oregon'},
+            {label: 'Pennsylvania', value: 'Pennsylvania'},
+            {label: 'Rhode Island', value: 'Rhode Island'},
+            {label: 'South Carolina', value: 'South Carolina'},
+            {label: 'South Dakota', value: 'South Dakota'},
+            {label: 'Tennessee', value: 'Tennessee'},
+            {label: 'Texas', value: 'Texas'},
+            {label: 'Utah', value: 'Utah'},
+            {label: 'Vermont', value: 'Vermont'},
+            {label: 'Virginia', value: 'Virginia'},
+            {label: 'Washington', value: 'Washington'},
+            {label: 'West Virginia', value: 'West Virginia'},
+            {label: 'Wisconsin', value: 'Wisconsin'},
+            {label: 'Wyoming', value: 'Wyoming'}
+        ],
         fullName: '',
         nickName: '',
         dateOfBirth: '',
@@ -69,74 +126,97 @@ export default class NewUserSignup extends Component {
 
     render() {
         return (
-            <ScrollView contentContainerStyle={styles.mainCol}>
-                <View style={styles.row1Label}>
-                    <Text style={styles.nameLabel}>Full Name</Text>
-                    <Text style={styles.nameLabel}>Nickname (Shown to others)</Text>
-                </View>             
-                <View style={styles.row1Input}>
-                    <TextInput style={styles.nameInput} value={this.state.fullName} onChange={(e) => this.setState({fullName: e.text})} />
-                    <TextInput style={styles.nameInput} value={this.state.nickName} onChange={(e) => this.setState({nickName: e.text})} />
-                </View>             
-                <View style={styles.row2Label}>
-                    <Text style={styles.dobLabel}>Date of Birth</Text>
-                    <Text style={styles.timeLabel}>Time of Birth</Text>
-                    <Text style={styles.genderLabel}>Gender</Text>
-                </View>             
-                <View style={styles.row2Input}>
-                    <Text style={styles.dobInput} value={this.state.dateOfBirth} onPress={this.datePicker}>{this.state.dateOfBirth}</Text>
-                    <Text style={styles.timeInput} value={this.state.timeOfBirth} onPress={this.timePicker}>{this.state.timeOfBirth}</Text>
-                    { this.state.show && <DateTimePicker
-                        value={this.state.date_time}
-                        mode={this.state.mode}
-                        is24Hour={false}
-                        display="default"
-                        onChange={this.setDate} />
-                    }
-                    <TextInput style={styles.genderInput} />
-                </View>             
-                <View style={styles.row3Label}>
-                    <Text style={styles.cityOfBirthLabel}>City of Birth</Text>
-                    <Text style={styles.stateOfBirthLabel}>State of Birth</Text>
-                </View>             
-                <View style={styles.row3Input}>
-                    <TextInput style={styles.cityOfBirthInput} />
-                    <TextInput style={styles.stateOfBirthInput} />
-                </View>             
-                <View style={styles.row4Label}>
-                    <Text style={styles.currentCityLabel}>Current City</Text>
-                    <Text style={styles.currentStateLabel}>Current State</Text>
-                </View>             
-                <View style={styles.row4Input}>
-                    <TextInput style={styles.currentCityInput} />
-                    <TextInput style={styles.currentStateInput} />
-                </View>             
-                <View style={styles.row5Label}>
-                    <Text style={styles.occupationLabel}>Occupation</Text>
-                    <Text style={styles.specialGiftLabel}>Special Gift</Text>
-                </View>             
-                <View style={styles.row5Input}>
-                    <TextInput style={styles.occupationInput} />
-                    <TextInput style={styles.specialGiftInput} />
-                </View>             
-                <View style={styles.row6Label}>
-                    <Text style={styles.emailLabel}>Email Address (username)</Text>
-                </View>             
-                <View style={styles.row6Input}>
-                    <TextInput style={styles.emailInput} />
-                </View>             
-                <View style={styles.row7Label}>
-                    <Text style={styles.passwordLabel}>Password</Text>
-                </View>             
-                <View style={styles.row7Input}>
-                    <TextInput style={styles.passwordInput} />
-                </View>             
-                <View style={styles.row8}>
-                    <TouchableOpacity style={styles.registerInput}>
-                        <Text style={styles.registerText}>Register</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
+            <KeyboardAvoidingView>
+                <ScrollView contentContainerStyle={styles.mainCol}>
+                    <View style={styles.row1Label}>
+                        <Text style={styles.nameLabel}>Full Name</Text>
+                        <Text style={styles.nameLabel}>Nickname (Shown to others)</Text>
+                    </View>             
+                    <View style={styles.row1Input}>
+                        <TextInput style={styles.nameInput} value={this.state.fullName} onChange={(e) => this.setState({fullName: e.text})} />
+                        <TextInput style={styles.nameInput} value={this.state.nickName} onChange={(e) => this.setState({nickName: e.text})} />
+                    </View>             
+                    <View style={styles.row2Label}>
+                        <Text style={styles.dobLabel}>Date of Birth</Text>
+                        <Text style={styles.timeLabel}>Time of Birth</Text>
+                        <Text style={styles.genderLabel}>Gender</Text>
+                    </View>             
+                    <View style={styles.row2Input}>
+                        <Text style={styles.dobInput} value={this.state.dateOfBirth} onPress={this.datePicker}>{this.state.dateOfBirth}</Text>
+                        <Text style={styles.timeInput} value={this.state.timeOfBirth} onPress={this.timePicker}>{this.state.timeOfBirth}</Text>
+                        { this.state.show && <DateTimePicker
+                            value={this.state.date_time}
+                            mode={this.state.mode}
+                            is24Hour={false}
+                            display="default"
+                            onChange={this.setDate} />
+                        }
+                        <Picker
+                            selectedValue={this.state.gender}
+                            style={styles.genderInput}
+                            onValueChange={(itemValue, itemIndex) =>
+                                this.setState({gender: itemValue})
+                            }>
+                            {this.state.genders.map((gender => <Picker.Item label={gender.label} value={gender.value} />))}
+                        </Picker>
+                    </View>             
+                    <View style={styles.row3Label}>
+                        <Text style={styles.cityOfBirthLabel}>City of Birth</Text>
+                        <Text style={styles.stateOfBirthLabel}>State of Birth</Text>
+                    </View>             
+                    <View style={styles.row3Input}>
+                        <TextInput style={styles.cityOfBirthInput} />
+                        <Picker
+                            selectedValue={this.state.stateOfBirth}
+                            style={styles.stateOfBirthInput}
+                            onValueChange={(itemValue, itemIndex) =>
+                                this.setState({stateOfBirth: itemValue})
+                            }>
+                            {this.state.states.map((state => <Picker.Item key={state.label} label={state.label} value={state.value} />))}
+                        </Picker>
+                    </View>             
+                    <View style={styles.row4Label}>
+                        <Text style={styles.currentCityLabel}>Current City</Text>
+                        <Text style={styles.currentStateLabel}>Current State</Text>
+                    </View>             
+                    <View style={styles.row4Input}>
+                        <TextInput style={styles.currentCityInput} />
+                        <Picker
+                            selectedValue={this.state.currentState}
+                            style={styles.currentStateInput}
+                            onValueChange={(itemValue, itemIndex) =>
+                                this.setState({currentState: itemValue})
+                            }>
+                            {this.state.states.map((state => <Picker.Item key={state.label} label={state.label} value={state.value} />))}
+                        </Picker>
+                    </View>             
+                    <View style={styles.row5Label}>
+                        <Text style={styles.occupationLabel}>Occupation</Text>
+                        <Text style={styles.specialGiftLabel}>Special Gift</Text>
+                    </View>             
+                    <View style={styles.row5Input}>
+                        <TextInput style={styles.occupationInput} value={this.state.occupation} onChange={(e) => this.setState({occupation: e.text})}/>
+                        <TextInput style={styles.specialGiftInput} value={this.state.specialGift} onChange={(e) => this.setState({specialGift: e.text})} />
+                    </View>             
+                    <View style={styles.row6Label}>
+                        <Text style={styles.emailLabel}>Email Address (username)</Text>
+                    </View>             
+                    <View style={styles.row6Input}>
+                        <TextInput style={styles.emailInput} value={this.state.email} onChange={(e) => this.setState({email: e.text})} />
+                    </View>             
+                    <View style={styles.row7Label}>
+                        <Text style={styles.passwordLabel}>Password</Text>
+                    </View>             
+                    <View style={styles.row7Input}>
+                        <TextInput style={styles.passwordInput} value={this.state.password} onChange={(e) => this.setState({password: e.text})} />
+                    </View>             
+                    <View style={styles.row8}>
+                        <TouchableOpacity style={styles.registerInput}>
+                            <Text style={styles.registerText}>Register</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
         )
     }
 }
