922d3340fb6ed53bdba12dc73a4d562c57919ca5
zeke13210 <zfarley94@gmail.com>
2020-03-04 09:52:54 -0500

    re add old launch screen and remove amplify files

--- meetmytypesmobile/amplify/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    cognito: {
-      REGION: 'us-east-1',
-      USER_POOL_ID: 'us-east-1_OlM4Wu5kC',
-      APP_CLIENT_ID: '5lrd7g5o19avhl8hl45s1ueqo3',
-      IDENTITY_POOL_ID: 'us-east-1:82644870-c417-4324-a60a-c32ba58588c9',
-    },
-}  
