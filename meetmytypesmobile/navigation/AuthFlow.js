ddda1bd239f13052608ac81e969fb759a1043d0f
Zach <zfarley94@gmail.com>
2020-05-02 19:21:36 -0400

    add working auth user flow

--- meetmytypesmobile/navigation/AuthFlow.js
+++ meetmytypesmobile/navigation/AuthFlow.js
@@ -5,33 +5,33 @@ import UserList from '../components/MainStack/userList/UserListScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState } from 'react';
 import MainAppNavigator from './MainAppNavigator';
 import LoginStack from './LoginNavigator'
 import AsyncStorage from '@react-native-community/async-storage';
 
-const state = {
-    isLoading: true,
-    userToken: null
-}
-function AuthFlow() {
 
-    React.useEffect(() => {
-        // Fetch the token from storage then navigate to our appropriate place
-        const bootstrapAsync = async () => {
-    
-          try {
-            state.userToken = await AsyncStorage.getItem('TOKEN');
-          } catch (e) {
-            // Restoring token failed
-          }
-        }
+function AuthFlow({ navigation}) {
+  const [isLoading, setLoadingState] = useState(true)
+  const [userToken, setUserToken] = useState(null)
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+
+    AsyncStorage.getItem('TOKEN').then(data => {
+      console.log("This is token: ", data)
+      //navigation.navigate('Profile')
+      setUserToken(data)
+    }).catch(err => {
+      console.log("Error recieving token: ", err)
     })
-    return (
-        <NavigationContainer>
-            {state.userToken == null ? (<LoginStack/>): (<MainAppNavigator/>)}
-        </NavigationContainer>
-    )
+
+
+  })
+  return (
+    <NavigationContainer>
+      {userToken == null ? (<LoginStack />) : (<MainAppNavigator />)}
+    </NavigationContainer>
+  )
 }
 
 export default AuthFlow;
