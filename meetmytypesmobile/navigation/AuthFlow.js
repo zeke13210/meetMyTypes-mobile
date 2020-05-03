7338eef41057e1e672fc77423dbd1451baef9aab
Zach <zfarley94@gmail.com>
2020-05-03 09:51:40 -0400

    remove unsed lib

--- meetmytypesmobile/navigation/AuthFlow.js
+++ meetmytypesmobile/navigation/AuthFlow.js
@@ -1,17 +1,11 @@
-import Profile from '../components/MainStack/Profile/ProfileScreen';
-import MatchRequest from '../components/MainStack/matchRequest/MatchRequestScreen';
-import ConfirmedMatch from '../components/MainStack/confirmedMatches/MatchScreen';
-import UserList from '../components/MainStack/userList/UserListScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
 import React, { useState } from 'react';
 import MainAppNavigator from './MainAppNavigator';
 import LoginStack from './LoginNavigator'
 import AsyncStorage from '@react-native-community/async-storage';
 
 
-function AuthFlow({ navigation}) {
+function AuthFlow() {
   const [isLoading, setLoadingState] = useState(true)
   const [userToken, setUserToken] = useState(null)
   React.useEffect(() => {
