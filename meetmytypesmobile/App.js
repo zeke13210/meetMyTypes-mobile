9089d4b7cbbcbf1a9473f6978067d2092a0b73a8
Zach <zfarley94@gmail.com>
2020-05-03 10:19:53 -0400

    add authflow to app.js

--- meetmytypesmobile/App.js
+++ meetmytypesmobile/App.js
@@ -12,60 +12,19 @@ import LoginNavigator from './navigation/LoginNavigator';
 import SplashScreen from './SplashScreen';
 import AuthFlow from './navigation/AuthFlow';
 
-import React from 'react';
-import {
-  StyleSheet
-} from 'react-native';
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
-import NewUserSignup from './NewUserSignup';
+export default class App extends Component {
 
-const App = () => {
-  return (
-    <>
-      <NewUserSignup />
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  constructor(props) {
+    super();
+    this.state = {
+      isLoading: true,
+      userToken: null
+    }
+  }
+  
+  render() {
+    const token = this.state.userToken
+    //return (token == null ? <LoginNavigator/> : <MainAppNavigator/> )
+    return (<AuthFlow/>)
+  }
+}
