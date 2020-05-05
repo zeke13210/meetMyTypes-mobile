fa339aeb39256f09c008d751defac953becb9bce
Zach <zfarley94@gmail.com>
2020-05-04 20:51:21 -0400

    correct 12 h - h time logic

--- meetmytypesmobile/components/LoginStack/Register/RegistrationScreen.js
+++ meetmytypesmobile/components/LoginStack/Register/RegistrationScreen.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import { Container, Content, Text, Input, Item, Button, Form, Icon, Header, Picker } from 'native-base';
 import { StyleSheet, View, TextInput, Image } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default function RegistrationScreen({ navigation }) {
     const [email, setEmail] = useState()
     const [gender, setGender] = useState(null)
     const [password, setPassword] = useState()
     const [dateTimeOfBirth, setDateOfBirth] = useState(new Date())
-    const [timeOfBirth, setTime] = useState('Please enter time')
-    const [dateOfBirth, setDate] = useState('Please enter date')
+    const [timeOfBirth, setTime] = useState('Time?')
+    const [dateOfBirth, setDate] = useState('Date?')
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
 
@@ -31,9 +32,25 @@ export default function RegistrationScreen({ navigation }) {
     const showTimepicker = () => {
         showMode('time');
     };
-    const convertDateToTime = () => {
-        let time = dateTimeOfBirth.getHours() + ":" + dateTimeOfBirth.getMinutes() //convert date to time string
-        setTime(time);
+    const convertDateToTime = (mode) => {
+        let time;
+        if ( dateTimeOfBirth.getHours() === 0){
+            time = (dateTimeOfBirth.getHours() + 12) + ":" + dateTimeOfBirth.getMinutes() + " AM" //convert date to time string
+
+        } else if ( dateTimeOfBirth.getHours() === 12){
+            console.log("HOurs: ", dateTimeOfBirth.getHours())
+            time = dateTimeOfBirth.getHours() + ":" + dateTimeOfBirth.getMinutes() + " PM" //convert date to time string
+        } else if (dateTimeOfBirth.getHours() > 12){
+            time = (dateTimeOfBirth.getHours() - 12) + ":" + dateTimeOfBirth.getMinutes() + " PM" //convert date to time string
+        } else {
+            time = dateTimeOfBirth.getHours() + ":" + dateTimeOfBirth.getMinutes() + " AM" //convert date to time string
+        }
+        let date = (dateTimeOfBirth.getMonth() + 1) + "/" + dateTimeOfBirth.getDate() + "/" + dateTimeOfBirth.getFullYear()
+        if (mode === 'time') {
+            setTime(time);
+        } else if (mode === 'date') {
+            setDate(date)
+        }
         setShow(false)
     }
     const resetScreen = () => {
@@ -47,78 +64,91 @@ export default function RegistrationScreen({ navigation }) {
                 <Text>To register fill out the form below</Text>
             </Header>
             <Content padder>
-                <Form>
+                <Grid>
+                    <Col>
+                        <Form>
+                            <Row>
+                                <Item regular style={styles.inputBox}>
+                                    <Input placeholder="email"
+                                        onChangeText={(text) => setEmail(text)}
+                                        placeholderTextColor="black"
+                                        value={email} />
+                                </Item>
+                            </Row>
+                            <Row>
+                                <Item regular style={styles.inputBox}>
+                                    <Input placeholder="password"
+                                        onChangeText={(text) => setPassword(text)}
+                                        placeholderTextColor="black"
+                                        secureTextEntry={true}
+                                        value={password} />
+                                </Item>
+                            </Row>
+                                <Item >
 
-                    <Item regular style={styles.inputBox}>
-                        <Input placeholder="email"
-                            onChangeText={(text) => setEmail(text)}
-                            placeholderTextColor="black"
-                            value={email} />
-                    </Item>
-                    <Item regular style={styles.inputBox}>
-                        <Input placeholder="password"
-                            onChangeText={(text) => setPassword(text)}
-                            placeholderTextColor="black"
-                            secureTextEntry={true}
-                            value={password} />
-                    </Item>
+                                    <Text style={{paddingRight: 10}}>Date of Birth</Text>
 
-                    
-                    <Item>
-                        <Text>Date of Birth</Text>
-                        <Icon active name="ios-calendar"/>
-                        <Text onPress={showDatepicker}>
-                            {dateOfBirth}
-                        </Text>
-                    </Item>
-                    <Item>
-                        <Text>Time of Birth</Text>
-                        <Icon active name='ios-alarm' />
-                        <Text onPress={showTimepicker}>
-                            {timeOfBirth}
-                        </Text>
-                    </Item>
-                    <Item picker>
-                        <Text>Gender</Text>
+                                    <Icon active name="ios-calendar" />
+                                    <Text style={styles.dateStyle} onPress={showDatepicker}>
+                                        {dateOfBirth}
+                                    </Text>
 
-                        <Picker
-                            mode="dropdown"
-                            iosIcon={<Icon name="arrow-down" />}
-                            style={{ width: undefined }}
-                            placeholder="Gender"
-                            placeholderStyle={{ color: "#bfc6ea" }}
-                            placeholderIconColor="#007aff"
-                            selectedValue={gender}
-                            onValueChange={(text) => setGender(text)}
-                        >
-                            <Picker.Item label="Male" value="Male" />
-                            <Picker.Item label="Female" value="Female" />
-                        </Picker>
-                    </Item>
+                                </Item>
+                            
+                                <Item>
+                                    <Text style={{paddingRight: 10}}>Time of Birth</Text>
+                                    <Icon active name='ios-alarm' />
+                                    <Text style={styles.dateStyle} onPress={showTimepicker}>
+                                        {timeOfBirth}
+                                    </Text>
+                                </Item>
+                            
 
-                    {show &&
-                    //need to confirm how to execute render and show for confirm
-                        (<Container>
-                            <DateTimePicker
-                                testID="dateTimePicker"
-                                //timeZoneOffsetInMinutes={0}
-                                value={dateTimeOfBirth}
-                                mode={mode}
-                                display="default"
-                                onChange={onChange}
-                            />
-                            <Button onPress={() => convertDateToTime()}>
-                                <Text>Confirm</Text>
+
+
+                            <Item picker>
+                                <Text>Gender</Text>
+                                <Picker
+                                    mode="dropdown"
+                                    iosIcon={<Icon name="arrow-down" />}
+                                    style={{ width: undefined }}
+                                    placeholder="Gender"
+                                    placeholderStyle={{ color: "#bfc6ea" }}
+                                    placeholderIconColor="#007aff"
+                                    selectedValue={gender}
+                                    onValueChange={(text) => setGender(text)}
+                                >
+                                    <Picker.Item label="Male" value="Male" />
+                                    <Picker.Item label="Female" value="Female" />
+                                </Picker>
+                            </Item>
+
+                            {show &&
+                                //need to confirm how to execute render and show for confirm
+                                (<Container>
+                                    <DateTimePicker
+                                        testID="dateTimePicker"
+                                        //timeZoneOffsetInMinutes={0}
+                                        value={dateTimeOfBirth}
+                                        mode={mode}
+                                        display="default"
+                                        onChange={onChange}
+                                    />
+                                    <Button onPress={() => convertDateToTime(mode)}>
+                                        <Text>Confirm</Text>
+                                    </Button>
+                                </Container>)
+                            }
+                            <Button block primary onPress={() => resetScreen()}>
+                                <Text>Clear Input</Text>
                             </Button>
-                        </Container>)
-                    }
-                    <Button block primary onPress={() => resetScreen()}>
-                        <Text>Clear Input</Text>
-                    </Button>
-                    <Button block primary onPress={() => navigation.goBack()}>
-                        <Text>Go back</Text>
-                    </Button>
-                </Form>
+                            <Button block primary onPress={() => navigation.goBack()}>
+                                <Text>Go back</Text>
+                            </Button>
+                        </Form>
+                    </Col>
+
+                </Grid>
 
             </Content>
 
@@ -134,5 +164,11 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         fontSize: 16,
         marginVertical: 10,
+    },
+
+    dateStyle: {
+        fontSize: 16,
+        borderRadius: 2,
+        padding: 8
     }
 })
