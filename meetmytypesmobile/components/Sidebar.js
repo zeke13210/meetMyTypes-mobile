6249b36e9b4eb974a0cfce648979447ac5677efc
Zach <zfarley94@gmail.com>
2020-05-14 09:35:34 -0400

    sider bar test

---
+++ meetmytypesmobile/components/Sidebar.js
@@ -0,0 +1,16 @@
+
+import { StyleSheet, View, TextInput, Image } from 'react-native';
+import { Text, Button } from 'native-base';
+import React, { useState, useEffect } from 'react';
+
+
+export default function Sidebar(){
+    return(
+        <View>
+            <Button>
+                <Text>Logout</Text>
+            </Button>
+        </View>
+    )
+
+}
