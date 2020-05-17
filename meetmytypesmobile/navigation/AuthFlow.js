c10d8510163ed2a5dd02d909f99c3e42b907325a
Zach <zfarley94@gmail.com>
2020-05-16 21:52:35 -0400

    add auth component

--- meetmytypesmobile/navigation/AuthFlow.js
+++ meetmytypesmobile/navigation/AuthFlow.js
@@ -1,70 +1,67 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
-import MainAppNavigator from './MainAppNavigator';
 import AsyncStorage from '@react-native-community/async-storage';
 import { Root } from 'native-base'
-import CustomDrawer from './DrawerNavigator';
-import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
-import LoginScreen from '../components/LoginStack/Login/LoginScreen';
-import RegistrationScreen from '../components/LoginStack/Register/RegistrationScreen';
-import AuthConext, { AuthContext } from './context'
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { AuthContext } from './context'
+import LoginNavigator from './LoginNavigator'
+import DrawerNavigator from './DrawerNavigator';
 
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
-function LoginNavigator(props) {
-  return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegistrationScreen} />
-    </Stack.Navigator>
-  );
-}
 function AuthFlow() {
   const [isLoading, setLoadingState] = useState(true)
   const [userToken, setUserToken] = useState(null)
 
   const authContext = React.useMemo(() => {
     return {
-      signIn: () => {
+      signIn: (token) => {
         setLoadingState(false);
-        setUserToken("ksjeoisjfso")
+        setUserToken(token)
       },
       signOut: () => {
         setUserToken(null)
-      }
+      },
+
     }
   }, [])
-  /*useEffect(() => {
+
+  useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
     console.log("Calling subscription function ")
 
-    AsyncStorage.getItem('TOKEN').then(data => {
-      console.log("This is token: ", data)
-      //navigation.navigate('Profile')
-      setUserToken(data)
-    }).catch(err => {
-      console.log("Error recieving token: ", err)
-    })
-  }, [userToken, setUserToken]) */
+    if (userToken === null) {
+      async function getToken() {
+        try {
+          let token = await AsyncStorage.getItem('TOKEN')
+          setUserToken(token)
+        } catch (e) {
+          console.log("Error getting token: ", e)
+        }
+      }
+      getToken()
+    } else {
+      console.log("storing token")
+      const storeToken = async () => {
+        try {
+          let res = await AsyncStorage.setItem('TOKEN', userToken)
+          console.log("storing res: ", res)
+
+        } catch (e) {
+          console.log("Error storing token: ", e)
+        }
+      }
+      storeToken()
+    }
+
+  }, [userToken])
+
   return (
     <Root>
       <AuthContext.Provider value={authContext}>
         <NavigationContainer>
-          {userToken == null ?
-            (<Stack.Navigator initialRouteName="Login">
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Register" component={RegistrationScreen} />
-            </Stack.Navigator>) :
-            (<Drawer.Navigator drawerType="slide" drawerContent={(props) => <CustomDrawer {...props} />}>
-              <Drawer.Screen name="Main" component={MainAppNavigator} />
-            </Drawer.Navigator>)}
-          {/**/}
+          {userToken == null ? (<LoginNavigator />) : (<DrawerNavigator />)}
         </NavigationContainer>
       </AuthContext.Provider>
     </Root>
-
   )
 }
 
