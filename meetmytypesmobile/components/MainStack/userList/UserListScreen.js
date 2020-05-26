4745cb8ea579f46e11bf6539ad142e93ba5d6d31
Zach <zfarley94@gmail.com>
2020-05-26 07:50:03 -0400

    add mapping for users

--- meetmytypesmobile/components/MainStack/userList/UserListScreen.js
+++ meetmytypesmobile/components/MainStack/userList/UserListScreen.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Content, Text, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import { Container, Content, Text, Header, Left, List, ListItem, Thumbnail, Body, Right, Button, Icon, Title } from 'native-base';
+import axios from 'axios';
 
-export default class UserListScreen extends Component {
-    render() {
-        return (
-            <Container>
-                <Header>
-                    <Left>
-                        <Button transparent>
-                            <Icon onPress={() => this.props.navigation.toggleDrawer()} name="md-menu" />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>{this.props.route.name}</Title>
-                    </Body>
-                    <Right/>
-                </Header>
-                <Content contentContainerStyle={styles.container}>
-                    <Text>user list screen Component</Text>
-                </Content>
-            </Container>
-        )
-    }
+export default function UserListScreen(props) {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        const pullData = async () => {
+            let response = await axios.get('https://q1jp3exnqb.execute-api.us-east-1.amazonaws.com/dev/admin/currentUsers')
+            setUsers(response.data.Items)
+            console.log("Users response: ", response.data.Items)
+        }
+        pullData()
+    }, [])
+    return (
+        <Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon onPress={() => props.navigation.toggleDrawer()} name="md-menu" />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>{props.route.name}</Title>
+                </Body>
+                <Right />
+            </Header>
+            <Content>
+                <List>
+                    {users.map(user => (
+                        <ListItem avatar>
+                            <Left>
+                                <Thumbnail source={require('../../../assets/empty.png')} />
+                            </Left>
+                            <Body>
+                                <Text>{user.Nickname}</Text>
+                                <Text note>{user.LoveType}</Text>
+                            </Body>
+                            <Right>
+                                <Text note>3:43 pm</Text>
+                            </Right>
+                        </ListItem>
+                    ))}
+
+                </List>
+                
+            </Content>
+        </Container>
+    )
+
 }
 
 const styles = StyleSheet.create({
