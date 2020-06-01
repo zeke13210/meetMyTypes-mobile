80932be1a8408285b1f40a5260069dd1a3488f6d
Zach <zfarley94@gmail.com>
2020-05-31 20:29:34 -0400

    remove all hears in stack

--- meetmytypesmobile/components/MainStack/userList/UserListStack.js
+++ meetmytypesmobile/components/MainStack/userList/UserListStack.js
@@ -10,14 +10,11 @@ const Stack = createStackNavigator();
 
 export default function UserListStack(props) {
     return (
-        <Stack.Navigator initialRouteName="UserList">
+        <Stack.Navigator initialRouteName="UserList" headerMode="none">
             <Stack.Screen name="UserList" component={UserListScreen} options={{ headerShown: false }} />
             <Stack.Screen name="ListProfile"
                 component={ListProfileScreen}
-                options={{
-                    headerTitle: 'List'
-                    
-                }} />
+                />
         </Stack.Navigator>
     );
 }
